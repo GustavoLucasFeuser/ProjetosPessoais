14a703ee54708bd9526d711ba92e74fd7474cc01
Gustavo Lucas Feuser <feuserfeuser2006@gmail.com>
2024-04-09 23:23:45 -0300

    cadastro pronto que joga para um json

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <!DOCTYPE html>
+<?php
+$user = isset($_POST['user']) ? $_POST['user'] : "";
+$senha = isset($_POST['senha']) ? $_POST['senha'] : "";
+$csenha = isset($_POST['csenha']) ? $_POST['csenha'] : "";
+?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -18,13 +23,44 @@
         <legend style="color:white;">Usuário</legend>
         <input class="form-control" type="text" placeholder="Insira o usuário" name="user" id="user">
         <legend style="color:white;">Senha</legend>
-        <input class="form-control" type="text" placeholder="Insira a senha" name="senha" id="senha">
+        <input class="form-control" type="password" placeholder="Insira a senha" name="senha" id="senha">
         <legend style="color:white;">Confirmar senha</legend>
-        <input class="form-control" type="text" placeholder="Confirmar senha" name="csenha" id="csenha">
+        <input class="form-control" type="password" placeholder="Confirmar senha" name="csenha" id="csenha">
         </div>
         <button type="submit" class="btn btn-light" style="margin-top:8%; margin-left: 70%;">Cadastrar</button>
     </form>
     </div>
     </div>
+
+    <?php
+        if(isset($_POST['user']) && ($_POST['senha']) && ($_POST['csenha'])) {
+
+         $dados = array(
+          'user' => $_POST['user'],
+          'senha' => $_POST['senha'],
+          'csenha' => $_POST['csenha']);
+
+        $dados_json = json_encode($dados);
+
+        $fp = fopen("informacoes.json", "w");
+
+        fwrite($fp, $dados_json);
+
+        fclose($fp);
+         }
+    ?>
+
+
+    <?php
+       if(isset($_POST['senha']) && ($_POST['senha'] === $_POST['csenha'])) {
+
+        header('Location: login.php');
+
+      }
+      else if(isset($_POST['senha'])){ //se forem diferentes mas veio do formulario
+          echo "<script>alert('As senhas não conhecidem')</script>";
+      }
+
+    ?>
 </body>
 </html>
