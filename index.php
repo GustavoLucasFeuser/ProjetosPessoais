0fc677bee7d4e280ae071e965316f285744a9e69
Gustavo Lucas Feuser <feuserfeuser2006@gmail.com>
2024-04-09 23:00:29 -0300

    termino do formulário com um front-end básico

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tarefas a fazer</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <h1 style="text-align: center;">Cadastre-se</h1><hr>
+
+    <div style="background-color: black; width: 400px; height: 400px; border-radius: 10px; margin-left:37%; margin-top: 7%; position: fixed;">
+        <div class="col-md-10">
+    <form action="" method="post">
+        <h3 style="color: white; margin-left: 35%; margin-top: 2%;">Cadastre-se</h3>
+        <div style="margin-left: 20%; margin-top: 10%;">
+        <legend style="color:white;">Usuário</legend>
+        <input class="form-control" type="text" placeholder="Insira o usuário" name="user" id="user">
+        <legend style="color:white;">Senha</legend>
+        <input class="form-control" type="text" placeholder="Insira a senha" name="senha" id="senha">
+        <legend style="color:white;">Confirmar senha</legend>
+        <input class="form-control" type="text" placeholder="Confirmar senha" name="csenha" id="csenha">
+        </div>
+        <button type="submit" class="btn btn-light" style="margin-top:8%; margin-left: 70%;">Cadastrar</button>
+    </form>
+    </div>
+    </div>
+</body>
+</html>
